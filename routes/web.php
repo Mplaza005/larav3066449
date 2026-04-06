f3421106781a5c2c6619b8d9021ed4ff1cf509b3
Manuel Alejandro <mplaza005@gmail.com>
2026-04-06 10:51:53 -0500

    primer commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OperacionesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('sumar/{num1}/{num2}',[OperacionesController::class,'sumar']);
+
+Route::get('restar',[OperacionesController::class,'restar']);
